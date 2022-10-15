97846c12094d7c0480875a7480302b8ffc281db0
sayan31paul <93469993+sayan31paul@users.noreply.github.com>
2022-10-15 12:23:58 +0530

    Add files via upload

---
+++ First program.py
@@ -0,0 +1,25 @@
+print("Hello world")
+print("Hello world 6")
+import flask
+import pandas
+
+#Multi line comment
+#Triple quote helps in writing multi line comment
+#Triple line comment can be written in double quotes.
+"""This is a multi line comment
+You can add as many number of lines in it."""
+
+print("Subscribe code with harry")
+print("Bhai video bhi like kar dena")
+"""The two print statements are coming in new line. Lets see how they come one after another."""
+
+print("Subscribe to code with Harry.",end="")
+print("Bhai video bhi like kar dena.")
+
+"""Lets see how to add space between the two lines."""
+print("Subscribe to code with Harry","Bahi video like bhi kar dena",end=" ")
+print("\n")
+
+"""\n is used to print new line."""
+print("C:\narry") #Inn this case we are not getting the desired output. Here we will have to use \\n yo get the desired output
+print("C:\\narry")
